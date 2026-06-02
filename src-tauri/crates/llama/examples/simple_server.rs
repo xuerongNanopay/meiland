1e7eda34fbf0ee7a6fb3825410bf878d3b085286
xuerongNanopay <xuerong.aws.ca@gmail.com>
2026-06-02 19:44:31 -0400

    add simple llama server

--- src-tauri/crates/llama/examples/simple_server.rs
+++ src-tauri/crates/llama/examples/simple_server.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("simple server");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+async fn server_description() -> impl Responder {
+    HttpResponse::Ok().body("Simple Ollama Server")
+}
+
+#[get("/complete")]
+async fn llama_complete() -> impl Responder {
+    HttpResponse::Ok().body("TODO: llama_complete")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let hostname = "127.0.0.1";
+    let port = 8080;
+
+    println!("Help: `curl http://{hostname}:{port}`");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(server_description)
+            .service(llama_complete)
+    })
+    .bind((hostname, port))?
+    .run()
+    .await
 }
