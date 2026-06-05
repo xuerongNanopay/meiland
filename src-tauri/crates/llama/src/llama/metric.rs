44d6c3ee9c0e94a0573f2c3d6a9d5dc695a7856c
xuerongNanopay <xuerong.aws.ca@gmail.com>
2026-06-05 16:28:21 -0400

    add llama metrics

--- src-tauri/crates/llama/src/llama/metric.rs
+++ src-tauri/crates/llama/src/llama/metric.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Default, Clone, Copy)]
+use std::time::SystemTime;
+
+#[derive(Debug, Clone, Copy)]
 struct LlamaMetrics {
     t_start: i64,
 
@@ -17,4 +19,28 @@ struct LlamaMetrics {
 
     n_decode_total: u64,
     n_busy_slots_total: u64,
+}
+
+impl Default for LlamaMetrics {
+    fn default() -> Self {
+        let t_start = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as i64;
+
+        Self {
+            t_start,
+            n_prompt_tokens_processed_total: 0,
+            t_prompt_processing_total: 0,
+            n_tokens_predicted_total: 0,
+            t_tokens_generation_total: 0,
+            n_tokens_max: 0,
+            n_prompt_tokens_processed: 0,
+            t_prompt_processing: 0,
+            n_tokens_predicted: 0,
+            t_tokens_generation: 0,
+            n_decode_total: 0,
+            n_busy_slots_total: 0,
+        }
+    }
 }
