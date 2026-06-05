ccb68b3c0936a9bd4ba793e493b6722f54d7db40
xuerongNanopay <xuerong.aws.ca@gmail.com>
2026-06-05 18:37:14 -0400

    add llm

--- src-tauri/crates/llama/src/llama/inferencer.rs
+++ src-tauri/crates/llama/src/llama/inferencer.rs
@@ -1,11 +1,32 @@
+use std::path::PathBuf;
+
 use llama_cpp_2::{
     context::LlamaContext, llama_backend::LlamaBackend, llama_batch::LlamaBatch, model::LlamaModel,
 };
 
+enum ModelType {
+    Text,
+    Mtmd,
+}
+
+#[derive(Debug, Clone)]
+struct LLMModel {
+    model_name: String,
+    model_path: PathBuf,
+    has_mtmd: bool,
+}
+
+
 struct InferenceTask {}
 
 struct LlamaInferencer {
+    model_meta: LLMModel,
+
     llama_model: LlamaModel,
     llama_backend: LlamaBackend,
     llama_batch: LlamaBatch<'static>,
+}
+
+impl LlamaInferencer {
+
 }
