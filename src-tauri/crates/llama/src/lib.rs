1c962f9b60dbf1613ff403b08832b82348039249
xuerongNanopay <xuerong.aws.ca@gmail.com>
2026-06-05 15:57:41 -0400

    save change

--- src-tauri/crates/llama/src/lib.rs
+++ src-tauri/crates/llama/src/lib.rs
@@ -1,4 +1,56 @@
+use std::path::PathBuf;
+
+use llama_cpp_2::{context::LlamaContext, llama_backend::LlamaBackend, model::LlamaModel};
+
+enum ModelType {
+    Text,
+    Mtmd,
+}
+
+#[derive(Debug, Clone)]
+struct LLMModel {
+    model_name: String,
+    model_path: PathBuf,
+    has_mtmd: bool,
+}
+
+/**
+ * Inference runtime
+ * 
+ * owns model/runtime state
+ * owns request/result queues
+ * owns active slots
+ * runs the inferece loop
+ * 
+ * TODO:
+ *  1. load model
+ *  2. start loop
+ *  3. terminate
+ * 
+ * 
+ */
+struct LlamaInference {
+    llm_model: LLMModel,
+}
+
+impl LlamaInference {
+
+
+}
+
+struct LLamaInternal {
+    llama_model: LlamaModel,
+    llama_backend: LlamaBackend,
+    llama_context: LlamaContext,
+
+}
+
+struct InferenceTask {
+    id: i32,
+    index: i32,
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 }
