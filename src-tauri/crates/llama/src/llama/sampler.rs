0b095a34150e79ace2d1968455798dee262c80a0
xuerongNanopay <xuerong.aws.ca@gmail.com>
2026-06-06 17:36:28 -0400

    add sample config

--- src-tauri/crates/llama/src/llama/sampler.rs
+++ src-tauri/crates/llama/src/llama/sampler.rs
@@ -1,8 +1,26 @@
 #[derive(Debug, Clone, Copy)]
-struct Sampleronfig {
+struct SamplerConfig {
+    temperature: f64, // <= 0.0 to sample greedily,
+    top_k: i32, // <= 0 to use vocab size
+    top_p: f64, // 1.0 = disabled
+    min_p: f64, // 0.0 = disabled
+}
 
+impl Default for SamplerConfig {
+    fn default() -> Self {
+        Self {
+           temperature: 0.8,
+           top_k: 40,
+           top_p: 0.95,
+           min_p: 0.05,
+        }
+    }
 }
 
 struct SamplerBuilder {
     
+}
+
+struct Sampler {
+
 }
