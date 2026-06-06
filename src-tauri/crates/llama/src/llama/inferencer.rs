63bc71abe33792b682efe6a2d7ffea2370b89fe0
xuerongNanopay <xuerong.aws.ca@gmail.com>
2026-06-05 22:02:07 -0400

    gen_llama_context

--- src-tauri/crates/llama/src/llama/inferencer.rs
+++ src-tauri/crates/llama/src/llama/inferencer.rs
@@ -1,7 +1,7 @@
-use std::{default, path::PathBuf};
+use std::{default, num::NonZeroU32, path::PathBuf};
 
 use llama_cpp_2::{
-    context::LlamaContext, llama_backend::LlamaBackend, llama_batch::LlamaBatch, model::{LlamaModel, params::LlamaModelParams},
+    context::{LlamaContext, params::LlamaContextParams}, llama_backend::LlamaBackend, llama_batch::LlamaBatch, model::{LlamaModel, params::LlamaModelParams},
 };
 
 enum ModelType {
@@ -28,15 +28,18 @@ struct InferenceTask {}
 
 struct LlamaInferencer {
     // model_meta: LLMModel,
+    config: InferenceConfig,
 
+    // Lora addaptor
     llama_model: LlamaModel,
     llama_backend: LlamaBackend,
+    // llama_vocab: LlamaVocab
     // llama_batch: LlamaBatch<'static>,
 }
 
 impl LlamaInferencer {
     
-    fn new(config: InferenceConfig) -> Result<Self, String>{
+    fn new(config: InferenceConfig) -> Result<Self, String> {
 
         let mut llama_backend = LlamaBackend::init()
             .map_err(|e| format!("Init Llama Backend Error: {e}"))?;
@@ -47,7 +50,7 @@ impl LlamaInferencer {
 
         let model_params = LlamaModelParams::default();
 
-        let model_path = PathBuf::from(config.model_path);
+        let model_path = PathBuf::from(&config.model_path);
 
         let llama_model = LlamaModel::load_from_file(
                 &llama_backend, 
@@ -57,8 +60,20 @@ impl LlamaInferencer {
             .map_err(|e| format!("Init Llama Model Error: {e}"))?;
     
         Ok(Self {
+            config,
             llama_backend,
             llama_model,
         })
+    }
+
+    fn gen_llama_context(&self) -> Result<Self, String> {
+    
+        let ctx_params = LlamaContextParams::default()
+            .with_n_ctx(NonZeroU32::new(11)) // set context window
+            .with_n_batch(11);
+        let llama_context = self.llama_model.new_context(&self.llama_backend, ctx_params)
+            .map_err(|e| format!("Init Llama Context Error: {e}"))?;
+
+        Err("TODO".to_owned())
     }
 }
